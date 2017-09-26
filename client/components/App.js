3d50436ea78ff241ff358135b2ffec570fa3e77e
Dominic Ma <dominic.c.ma@gmail.com>
2017-09-25 19:59:20 -0700

    Set up base React functionality

---
+++ client/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render ()  {
+    return (
+      <h1>Hello from react</h1>
+    );
+  }
+
+}
